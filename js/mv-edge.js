6f11fa3a26e60b950fca667789457b7c9ed3f41c
Damon Maldonado <damon@mpwmarketing.com>
2016-09-16 15:09:53 -0400

    change scripts

---
+++ js/mv-edge.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function () { var navigation = responsiveNav(".nav-collapse", {
+        // Close the navigation when it's tapped
+        closeOnNavClick: true
+    });
+     });
