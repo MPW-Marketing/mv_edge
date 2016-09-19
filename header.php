b2284b996273b15d2b7c651ab613417401c89ba9
Damon Maldonado <damon@mpwmarketing.com>
2016-09-19 15:38:37 -0400

    no async

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <script src="https://use.typekit.net/ltp3bfu.js"></script>
-<script>try{Typekit.load({ async: false });}catch(e){}</script>
+<script>try{Typekit.load({  });}catch(e){}</script>
 <?php wp_head(); ?>
 </head>
 
