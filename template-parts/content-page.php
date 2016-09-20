5a153c3f3f224b444934249a097a0d3ac3baa403
Damon Maldonado <damon@mpwmarketing.com>
2016-09-20 16:44:18 -0400

    fixed

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,7 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php  $hide_title = get_field($hide_heading);
+    <?php  $hide_title = get_field( 'hide_heading' );
     if ( $hide_title !== 'Yes'){ ?>
 	<header class="entry-header content-width">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
